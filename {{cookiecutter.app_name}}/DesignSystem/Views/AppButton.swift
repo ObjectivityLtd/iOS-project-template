aa7cdddc3a02eddd929b907906a696e22071f13c
pgorzelany <pgorzelany@objectivity.co.uk>
2019-12-05 21:04:51 +0100

    Added Resources folder to design system framework

---
+++ {{cookiecutter.app_name}}/DesignSystem/Views/AppButton.swift
@@ -0,0 +1,21 @@
+//
+//  AppButton.swift
+//  DesignSystem
+//
+//  Created by pgorzelany on 05/12/2019.
+//  Copyright © 2019 {{cookiecutter.company_name}}. All rights reserved.
+//
+
+import UIKit
+
+class AppButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
